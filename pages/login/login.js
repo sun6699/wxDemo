7e266160a74a6eccb1077f741012658ba0bbda07
孙 <1595878969@qq.com>
2018-07-18 17:09:02 +0800

    测试

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,55 +1,78 @@
 //index.js
 //获取应用实例
 const app = getApp()
+
 Page({
   data: {
-    name: "",
-    pwd: "",
-    Listinfo: [{}],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  onLoad: function () {
-    var that = this
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-  textinput: function (e) {
-    var types = e.currentTarget.dataset.type;
-    if (types == 1) {
+  onLoad: function() {
+    if (app.globalData.userInfo) {
       this.setData({
-        name: e.detail.value
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
     } else {
-      this.setData({
-        pwd: e.detail.value
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
       })
     }
-
   },
-  login: function (e) {
-    var that=this
-    var name=that.data.name
-    var pwd=that.data.pwd
+  login: function() {
     wx.login({
-      success: function (res) {
+      success: function(res) {
         if (res.code) {
           //发起网络请求
           wx.request({
             url: 'http://upliusun.vicp.io:12878/api/testwx.aspx/Login',
+            method:"post",
             data: {
-              name: name,
-              pwd: pwd,
-              code: res.code,
+              code: res.code
             },
-            method: "POST",
             header: {
               'content-type': 'application/json' // 默认值
             },
-            success: function (res) {
-              console.log(res.data.d)
+            success: function(res) {
+              var result = JSON.parse(res.data.d)
+              if (result[0].status == 0) {
+                wx.navigateTo({
+                  url: '../logs/logs',
+                })
+              } else {
+                wx.navigateTo({
+                  url: '../index/index',
+                })
+              }
             }
           })
         } else {
           console.log('登录失败！' + res.errMsg)
         }
       }
-    });
+    })
   }
 })
